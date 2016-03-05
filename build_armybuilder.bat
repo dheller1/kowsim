dfa014553c53353b34d223eede119ceaf2c29b6e
dheller1 <dominik.heller1@gmail.com>
2016-03-05 17:21:40 +0100

    - Added armybuilder build scripts.
    - Added some additional forces

---
+++ build_armybuilder.bat
@@ -0,0 +1,9 @@
+rm armybuilder.zip
+cd src
+python setup_ab.py py2exe
+cd ..
+move src\dist\* src\
+rmdir src\dist
+del /Q src\build\*
+rmdir /S /Q src\build
+"C:\Program Files\7-Zip\7z.exe" a armybuilder.zip "src" "data\kow" "data\icons" "data\*.csv" "armybuilder.bat"
